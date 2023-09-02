fd24238f77e6cb4ddc2557f494c6e2c423764f07
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-02 22:04:08 +0530

    Practice questions done

---
+++ Day 2/Questions/Script.js
@@ -0,0 +1,61 @@
+//1
+// let num = 57;
+// if(num%10 == 0){
+//     console.log("Good");
+// } else {
+//     console.log("Bad");
+// }
+
+//2
+// let name = prompt("Enter your name : ");
+// let age = prompt("Enter your age : ");
+// alert(` ${name} is ${age} years old.`);
+
+//3
+// let quarter = 2;
+// switch (quarter) {
+//     case 1 : console.log("January, February, March");
+//         break;
+//     case 2 : console.log("April, May, June");
+//         break;
+//     case 3 : console.log("July, August, September");
+//         break;
+//     case 4 : console.log("October, November, December");
+//         break;
+//     default : console.log("Wrong Input");
+// }
+
+//4
+// let string = "4arav";
+// if(string[0] === "A" || string[0] === "a" && string.length > 5){
+//     console.log("Golden String");
+// } else {
+//     console.log("Not a Golden String");
+// }
+
+//5
+// let a = 5;
+// let b = 6;
+// let c = 9;
+
+// if(a>b && a>c){
+//     console.log("Greatest num is : " + a);
+// } else if(b>a && b>c){
+//     console.log("Greatest num is : " + b);
+// } else {
+//     console.log("Greatest num is : " + c);
+// }
+
+//6
+// let a = 32;
+// let b = 47852;
+
+// let firstNo = a % 10;
+// let secondNo = b % 10;
+
+// if(firstNo == secondNo){
+//     console.log("Both number's last digit is same");
+// } else {
+//     console.log("Last digit not same");
+// }
+
