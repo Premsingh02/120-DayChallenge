eaeb4de8d2a1f6ea218a79910a34abc2816033aa
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-02 17:52:37 +0530

    Switch done

--- Day 2/Dev/Script.js
+++ Day 2/Dev/Script.js
@@ -49,14 +49,28 @@
 //     console.log("GO")
 // }
 
-let size = "M";
+// let size = "M";
 
-if(size === "XL"){
-    console.log("Price is Rs : 250");
-} else if (size === "L"){
-    console.log("Price is Rs : 200");
-} else if (size === "M"){
-    console.log("Price is Rs : 100");
-} else if (size === "S"){
-    console.log("Price is Rs : 50");
-}
+// if(size === "XL"){
+//     console.log("Price is Rs : 250");
+// } else if (size === "L"){
+//     console.log("Price is Rs : 200");
+// } else if (size === "M"){
+//     console.log("Price is Rs : 100");
+// } else if (size === "S"){
+//     console.log("Price is Rs : 50");
+// }
+
+//Switch
+// let color = "green";
+
+// switch(color) {
+//     case "red" : console.log("Stop");
+//         break;
+//     case "yellow" : console.log("Slow Down");
+//         break;
+//     case "green" : console.log("Go");
+//         break;
+//     default : console.log("Broken Light");
+
+// }
