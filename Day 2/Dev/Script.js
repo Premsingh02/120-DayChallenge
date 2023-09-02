e86c15e18b6ffe72636ee8bb6d42a07341726652
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-02 08:31:00 +0530

    Operators in JS Started

--- Day 2/Dev/Script.js
+++ Day 2/Dev/Script.js
@@ -5,10 +5,30 @@
 // let b = 5;
 // console.log("Sum is : " , a + b); // sum of a and b;
 
-let pencilPrice = 10;
-let erasorPrice = 5;
+// let pencilPrice = 10;
+// let erasorPrice = 5;
 // let totalPrice = "The total price is : " + (pencilPrice + erasorPrice) + " Rupees."
 
 // Template literals
-let output = `The total price is : ${pencilPrice + erasorPrice} Rupees.`; //Back ticks
-console.log(output);
+// let output = `The total price is : ${pencilPrice + erasorPrice} Rupees.`; //Back ticks
+// console.log(output);
+
+
+let a = 10;
+let b = 5;
+
+//Arithmetic Operators (+,-,*,/,%,**)
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//Unary Operators (++,--)
+console.log(a++);
+console.log(++a);
+
+//Assignment Operators (=,+=,-=,*=,/=,%=,etc);
+a = b;
+console.log(a);
