29248b7c5960b3c7bc512b76efc682301e951946
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-05 11:07:24 +0530

    Started day 5 with String's methods

---
+++ Day 5/Dev/script.js
@@ -0,0 +1,7 @@
+// let msg = "  Hello  ";
+// msg = msg.trim();
+// console.log(msg);
+
+// let password = prompt("Set Your Password");
+// let newPassword = password.trim();
+// console.log(newPassword);
