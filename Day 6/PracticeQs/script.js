e17e0d74409b237242587d8da4f682e713cee97a
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-06 19:09:06 +0530

    Practice Qs Done

---
+++ Day 6/PracticeQs/script.js
@@ -0,0 +1,10 @@
+//Q1
+// let msg = "help!";
+// let newMsg = msg.trim().toUpperCase();
+// console.log(newMsg);
+
+//Q3
+// let name = "ApnaCollege";
+// let newName = name.slice(4);
+// let newStr = newName.replace("l","t").replace("l","t");
+// console.log(newStr); 
