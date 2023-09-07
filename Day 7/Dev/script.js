51251fe1c48595ec8b02ad2563b57ebf858cdfbf
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-07 19:34:22 +0530

    array slice method done

---
+++ Day 7/Dev/script.js
@@ -0,0 +1,8 @@
+// Array Methods
+// .slice();
+// let colors = ["red","yellow","blue","orange","pink","white"];
+
+// colors.slice(); //it will return whole array;
+// colors.slice(2); //start from 2 index and go till end;
+// colors.slice(2,3); // 2 is starting index and 3 is ending index, 3 is excluded;
+// colors.slice(-2); //it will take last 2;
