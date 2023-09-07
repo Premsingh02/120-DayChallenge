16bbad7e69cafca28fd37612fa78e1fe69b1dd26
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-07 20:06:41 +0530

    more Practice question's done

--- Day 7/PracticeQs/script.js
+++ Day 7/PracticeQs/script.js
@@ -5,3 +5,14 @@
 // months.unshift("june");
 // months.unshift("july");
 // console.log(months);
+
+//Q2
+// let months = ["january","july","march","august"];
+// months.splice(0,2,"january","june");
+// console.log(months);
+
+//Q3
+// let arr = ["c","c++","html","javascript","python","java","c#","sql"];
+// arr.reverse();
+// console.log(arr);
+// console.log(arr.indexOf("javascript"));
