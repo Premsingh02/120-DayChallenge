a1e1e77fa8ac838b5fb07369a461c2c1215f2129
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-07 19:54:52 +0530

    array splice method done

--- Day 7/Dev/script.js
+++ Day 7/Dev/script.js
@@ -1,8 +1,15 @@
 // Array Methods
-// .slice();
+// 1) .slice();
 // let colors = ["red","yellow","blue","orange","pink","white"];
 
 // colors.slice(); //it will return whole array;
 // colors.slice(2); //start from 2 index and go till end;
 // colors.slice(2,3); // 2 is starting index and 3 is ending index, 3 is excluded;
 // colors.slice(-2); //it will take last 2;
+
+//2) .splice();
+// let colors = ["red","yellow","blue","orange","pink","white"];
+// colors.splice(4);
+// colors.splice(0,1);
+// colors.splice(0,1,"black","grey"); //black and grey will be added at the 0 index.
+// console.log(colors);
