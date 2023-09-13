7bd05229869f613f88ea07e2df3bd202249bba84
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-13 11:33:15 +0530

    conversion of key values to string done

--- Day 13/Dev/script.js
+++ Day 13/Dev/script.js
@@ -1,10 +1,18 @@
-const post = {
-    username: "@Aaravkumar.dev",
-    content: "This is my #firstPost",
-    likes: 150,
-    reposts: 5,
-    tags: ["@apnacollege", "@delta"],
-};
+// const post = {
+//     username: "@Aaravkumar.dev",
+//     content: "This is my #firstPost",
+//     likes: 150,
+//     reposts: 5,
+//     tags: ["@apnacollege", "@delta"],
+// };
 
-post["username"];
-post.username;
+// post["username"];
+// post.username;
+
+const obj = {
+    1: "a",
+    2: "b",
+    true: "c",
+    null: "d",
+    undefined: "e",
+}; // All the keys are converted into strings.
