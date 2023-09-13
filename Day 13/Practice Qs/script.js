bffcfeaf3ca6d83863ccb6b8815503b05924d1c5
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-13 19:58:55 +0530

    Practice Qs done

---
+++ Day 13/Practice Qs/script.js
@@ -0,0 +1,39 @@
+// Q1
+// let random = Math.floor(Math.random() * 100) + 1;
+// console.log(random);
+
+// Q2
+// let random = Math.floor(Math.random() * 5) + 1;
+// console.log(random);
+
+// Q3
+// Create a program that generates a random number representing a dice roll.
+// [The number should be between 1 and 6]
+// let diceRoll = Math.floor(Math.random() * 6) + 1;
+// console.log(diceRoll);
+
+// Q4
+// Create an object representing a car that stores the following properties for the
+// car: name, model, color.
+// Print the car’s name
+// const car = {
+//     name: "BMW",
+//     model: "i100",
+//     color: "Black",
+// };
+// console.log(car.name);
+
+// Q5
+// Create an object Person with their name, age and city.
+// Edit their city’s original value to change it to “New York”.
+// Add a new property country and set it to the United States.
+// const person = {
+//     name: "Aarav",
+//     age: 22,
+//     city: "Kolkata",
+// }
+
+// person.city = "New York";
+// person.country = "United States";
+
+// console.log(person);
