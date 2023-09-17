a547a91d62964b9fa1fc1fc7cd2fdcbd3df65bcb
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-17 10:21:17 +0530

    HOF returning a function done

--- DAy 17/Dev/script.js
+++ DAy 17/Dev/script.js
@@ -12,3 +12,20 @@
 
 // multipleGreet(greet,10);
 
+
+//HOF RETURNS A FUNCTION
+function oddEvenTest(request){
+    if(request == "odd") {
+        return function(n) {
+            console.log(!(n%2 == 0));
+        }
+    } else if (request == "even"){
+        return function(n) {
+            console.log(n%2 == 0);
+        }
+    } else {
+        console.log("wrong request");
+    }
+}
+
+let request = "odd";
