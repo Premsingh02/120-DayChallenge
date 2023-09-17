054611902e43f7ceccf36c70bae0599c77fad070
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-17 10:11:00 +0530

    HOF done

---
+++ DAy 17/Dev/script.js
@@ -0,0 +1,14 @@
+ //HOF
+// function multipleGreet(func,n) {
+//     for(let i=1; i<=n; i++){
+//         func();
+//     }
+// }
+
+
+// let greet = function() {
+//     console.log("Hello");
+// }
+
+// multipleGreet(greet,10);
+
