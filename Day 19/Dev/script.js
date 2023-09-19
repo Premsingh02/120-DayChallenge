5a71b27436919cf17dd9f8ab47cd7ef000e81079
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-19 18:43:17 +0530

    set timeout done

--- Day 19/Dev/script.js
+++ Day 19/Dev/script.js
@@ -1,31 +1,41 @@
 //this
-const student = {
-    name: "Aarav",
-    age: 22,
-    eng: 95,
-    math: 90,
-    phy: 97,
-    getAvg() {
-        let avg = (this.eng + this.math + this.phy) / 3;
-        console.log(`${this.name} got avg marks = ${avg}`);
-    }
-}
+// const student = {
+//     name: "Aarav",
+//     age: 22,
+//     eng: 95,
+//     math: 90,
+//     phy: 97,
+//     getAvg() {
+//         let avg = (this.eng + this.math + this.phy) / 3;
+//         console.log(`${this.name} got avg marks = ${avg}`);
+//     }
+// }
 
 
 //Try and catch
-try {
-    console.log(a); // a is not defined error
-} catch {
-    console.log("Variable is not defined");
-}
+// try {
+//     console.log(a); // a is not defined error
+// } catch {
+//     console.log("Variable is not defined");
+// }
 
-console.log("HEllo")
-console.log("HEllo")
-console.log("HEllo")
-console.log("HEllo")
+// console.log("HEllo")
+// console.log("HEllo")
+// console.log("HEllo")
+// console.log("HEllo")
 
 
 //Arrow Functions
-const sum = (a,b) => {
-    console.log(a+b);
-}
+// const sum = (a,b) => {
+//     console.log(a+b);
+// }
+
+
+//Set Timeout
+console.log("Hello");
+
+setTimeout( ()=> {
+    console.log("Aarav Kumar");
+}, 4000);
+
+console.log("Welcome");
