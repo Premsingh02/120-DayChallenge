2a66a187f96f2530b68e632954a4432382754635
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-20 11:50:28 +0530

    setInterval done

---
+++ Day 20/Dev/script.js
@@ -0,0 +1,12 @@
+//setInterval
+// let id = setInterval( () => {
+//     console.log("Apna College");
+// }, 2000);
+
+// console.log(id);  //to stop interval
+
+// let id2 = setInterval( () => {
+//     console.log("Hello World");
+// }, 3000);
+
+// console.log(id2);
