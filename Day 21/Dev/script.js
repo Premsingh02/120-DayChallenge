8e811341634a6d30c8e4a19d91cea5a10490aeff
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-21 16:40:52 +0530

    Filter done

--- Day 21/Dev/script.js
+++ Day 21/Dev/script.js
@@ -40,3 +40,14 @@
 // let double = num.map(function(el){
 //     return el * 2;
 // });
+
+//-----------------------------------------------------------
+
+//Filter - 
+// let nums = [2,4,1,5,6,2,7,8,9];
+
+// let even = nums.filter( (num) => {
+//     return num % 2 == 0;
+// })
+
+// console.log(even);
