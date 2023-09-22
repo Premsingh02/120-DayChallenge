ba4118b28b2fcdc23768f716be8e2ca13bd86561
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-22 17:49:56 +0530

    Spread, Array Literals

---
+++ DAy 22/Dev/script.js
@@ -0,0 +1,13 @@
+//Spread - 
+let arr = [1,2,3,4,5];
+let newArr = [...arr];
+console.log(newArr);
+
+let chars = [..."hello"];
+console.log(chars);
+
+let odd = [1,3,5,7,9];
+let even = [2,4,6,8,10];
+
+let nums = [...odd, ...even];
+console.log(nums);
