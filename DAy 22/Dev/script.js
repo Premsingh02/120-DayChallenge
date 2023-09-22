1c2b9f9c9ebd9340cb6ffbc380e4f6368085f96a
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-22 19:27:22 +0530

    Spread, Object Literals done

--- DAy 22/Dev/script.js
+++ DAy 22/Dev/script.js
@@ -11,3 +11,16 @@ let even = [2,4,6,8,10];
 
 let nums = [...odd, ...even];
 console.log(nums);
+
+//Spread with Object Literals -
+let data = {
+    email: "ironman@gmail.com",
+    password: "abcd",
+}
+
+let dataCopy = {...data, id: 123};
+console.log(dataCopy);
+
+let arr2 = [1,2,3,4,5];
+let obj = {...arr2};
+console.log(obj);
