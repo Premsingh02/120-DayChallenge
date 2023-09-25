43780b764b2327522c87218904ef204232443c29
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-25 19:25:04 +0530

    Using properties and methods

---
+++ Day 25/Dev/script.js
@@ -0,0 +1,7 @@
+// console.dir(document.querySelector('h1'));
+// console.dir(document.querySelector('#description'));
+// console.dir(document.querySelector('.boxLink'));
+
+// console.dir(document.querySelectorAll("div a"));
+
+let para = document.querySelector('p');
