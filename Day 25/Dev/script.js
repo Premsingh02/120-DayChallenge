b2e0821f47f285a399e78855c478f1c660c6b7ea
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-26 18:06:44 +0530

    some style property done

--- Day 25/Dev/script.js
+++ Day 25/Dev/script.js
@@ -4,4 +4,14 @@
 
 // console.dir(document.querySelectorAll("div a"));
 
-let para = document.querySelector('p');
+// let para = document.querySelector('p'); 
+
+let links = document.querySelectorAll(".box a");
+
+for(link of links) {
+    link.style.color = "red";
+}
+
+// for(let i = 0; i<links.length; i++){
+//     links[i].style.color = "green";
+// }
