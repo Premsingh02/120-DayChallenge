a9a1b4fc5dbbcfb05fae4e1e80df227928923464
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-28 19:07:21 +0530

    started my day 28

---
+++ Day 28/Practice Qs/script.js
@@ -0,0 +1,12 @@
+let para1 = document.createElement('p');
+para1.innerText = "Hey I'm red!";
+document.querySelector("body").append(para1);
+
+para1.classList.add('red');
+
+let h3 = document.createElement('h3');
+h3.innerText = "I'm a blue h3!";
+document.querySelector("body").append(h3);
+
+h3.classList.add('blue');
+
