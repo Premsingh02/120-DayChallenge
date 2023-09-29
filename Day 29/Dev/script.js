4a81efeb233f808170c9101534733de9f87e79de
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-29 21:44:35 +0530

    some changes done

--- Day 29/Dev/script.js
+++ Day 29/Dev/script.js
@@ -27,8 +27,11 @@ console.dir(btn);
 let btns = document.querySelectorAll("button");
 
 for(btn of btns) {
-    btn.addEventListener("click",sayHello);
-    btn.addEventListener("click",sayName);
+    // btn.addEventListener("click",sayHello);
+    // btn.addEventListener("click",sayName);
+    btn.addEventListener("dblclick",function () {
+        console.log("You double clicked me!");
+    });
 };
 
 function sayHello() {
