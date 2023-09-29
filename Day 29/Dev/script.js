c59247ae2ef4ee439295553c3b50468fa879ba9c
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-29 19:36:01 +0530

    starting DOM events

---
+++ Day 29/Dev/script.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector("button");
+console.dir(btn);
+
+// btn.onclick = function () { 
+//     alert("button was clicked");
+// }
+
+//         or
+
+function sayHello() {
+    alert("Hello!");
+}
+
+btn.onclick = sayHello;
