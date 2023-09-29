81dd2117776e51cf609ac621be7aa6f31ab19a61
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-29 10:54:33 +0530

    some practice qs done

---
+++ Day 29/Practice Qs/script.js
@@ -0,0 +1,20 @@
+let input = document.createElement("input");
+let btn = document.createElement("button");
+
+btn.innerText = "Click me";
+
+document.querySelector("body").prepend(input);
+document.querySelector("body").append(btn);
+
+input.placeholder = "username";
+btn.id = "btn";
+
+btn.classList.add("btn");
+
+let h1 = document.createElement("h1");
+h1.innerText = "DOM Practice";
+document.querySelector("body").append(h1);
+
+let p = document.createElement("p");
+p.innerHTML = "Apna College <b> Delta </b> Practice";
+document.querySelector("body").append(p);
