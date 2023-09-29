42ec2e0acdffa69f6f240c39702c13895813331b
Premsingh02 <theaaravkumar02@gmail.com>
2023-09-29 21:42:42 +0530

    EventListner done

--- Day 29/Dev/script.js
+++ Day 29/Dev/script.js
@@ -7,14 +7,34 @@ console.dir(btn);
 
 //         or
 
+// function sayHello() {
+//     alert("Hello!");
+// }
+
+// btn.onclick = sayHello;
+
+// function sayHi() {
+//     alert("HI!");
+// }
+
+// btn.onmouseenter = sayHi;
+
+// addEventListener 
+// element.addEventListener(event,callback);
+
+
+// EventListener -
+let btns = document.querySelectorAll("button");
+
+for(btn of btns) {
+    btn.addEventListener("click",sayHello);
+    btn.addEventListener("click",sayName);
+};
+
 function sayHello() {
     alert("Hello!");
 }
 
-btn.onclick = sayHello;
-
-function sayHi() {
-    alert("HI!");
+function sayName() {
+    alert("Aarav Kumar");
 }
-
-btn.onmouseenter = sayHi;
