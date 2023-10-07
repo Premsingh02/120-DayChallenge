5652f5ea899b221d7df91be00bb184dfd81dfa7b
Premsingh02 <theaaravkumar02@gmail.com>
2023-10-07 11:54:24 +0530

    this in event listeners done

--- Day 37/Dev/script.js
+++ Day 37/Dev/script.js
@@ -9,3 +9,18 @@
 // box.addEventListener("mouseenter", function() {
 //     console.log("mouse inside box");
 // });
+
+let btn = document.querySelector("button");
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3");
+let p = document.querySelector("p");
+
+function changeColor() {
+    console.dir(this.innerText);
+    this.style.backgroundColor = "blue";
+}
+
+btn.addEventListener("click", changeColor);
+h1.addEventListener("click", changeColor);
+h3.addEventListener("click", changeColor);
+p.addEventListener("click", changeColor);
